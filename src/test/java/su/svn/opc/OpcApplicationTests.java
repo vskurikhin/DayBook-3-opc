cf7ebd1e7bc8fa9968d54d64be249c666073c67d
Victor N. Skurikhin <vs@lnhost.net>
2021-01-08 21:48:46 +0300

    Second commit

---
+++ src/test/java/su/svn/opc/OpcApplicationTests.java
@@ -0,0 +1,13 @@
+package su.svn.opc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
